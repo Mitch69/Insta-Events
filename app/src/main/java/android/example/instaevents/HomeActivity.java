b0e2489923d4364ad50c90be131b0b82af8f87ac
fionachristina <38242986+fionachristina@users.noreply.github.com>
2019-07-06 13:54:34 +0300

    Updated

---
+++ app/src/main/java/android/example/instaevents/HomeActivity.java
@@ -0,0 +1,48 @@
+package android.example.instaevents;
+
+import android.animation.ArgbEvaluator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeActivity extends AppCompatActivity {
+    ViewPager viewPager;
+    android.example.viewpager.Adapter adapter;
+    List<android.example.viewpager.Model> models;
+    Integer[] colors = null;
+    ArgbEvaluator argbEvaluator = new ArgbEvaluator();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        models = new ArrayList<>();
+        models.add(new android.example.viewpager.Model(R.drawable.image1, "Brochure", "Brochure is an informative paper document (often also used for advertising) that can be folded into a template"));
+        models.add(new android.example.viewpager.Model(R.drawable.image2, "Sticker", "Sticker is a type of label: a piece of printed paper, plastic, vinyl, or other material with pressure sensitive adhesive on one side"));
+        models.add(new android.example.viewpager.Model(R.drawable.image3, "Poster", "Poster is any piece of printed paper designed to be attached to a wall or vertical surface."));
+        models.add(new android.example.viewpager.Model(R.drawable.image4, "Namecard", "Business cards are cards bearing business information about a company or individual."));
+
+        adapter = new android.example.viewpager.Adapter(models, this);
+
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+        viewPager.setPadding(130, 0, 130, 0);
+
+
+
+
+
+    }
+
+    public void btnSettings_onclick(View view) {
+        Intent intent = new Intent(this,SettingsActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/android/example/instaevents/ImageAdapter.java
@@ -0,0 +1,43 @@
+package android.example.instaevents;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class ImageAdapter extends PagerAdapter {
+    private Context mContext;
+    private int [] mImageIds = new int[]{R.drawable.image1, R.drawable.image2, R.drawable.image3, R.drawable.image4};
+
+    ImageAdapter(Context context){
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return mImageIds.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imageView = new ImageView(mContext);
+
+        imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        imageView.setImageResource(mImageIds[position]);
+        container.addView(imageView, 0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((ImageView)object);
+    }
+}
